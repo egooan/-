3ce8873fb850951d9e5afc73414ff3c337b119c5
dohyunlee09 <dohyunlee0916@gmail.com>
2026-09-19 11:17:35 +0900

    간단한 변수연습

--- 0 environmental.py
+++ 0 environmental.py
@@ -1,3 +1,16 @@
-print(123)
-print(1234)
-print(12345)
+a=123
+print(a)
+
+a='a'
+print(a)
+
+a=123+123
+print(a)
+a=[1123,"sasd", 3, 5+3]
+print(a)
+var1=10
+var2=20
+a=var1+var2
+print(a)
+input_var = input("숫자를 하나 입력하시오")
+print(input_var)
